5844644a1fb12a170005ba4f4df321cd4225e3a3
jessieconnralphsam <jessieconnralph.official@gmail.com>
2024-09-03 22:02:51 +0800

    try

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,9 +1,19 @@
 <template>
-   Add vue js to laravel sample
+  <h1>{{ message }}</h1>
+  <h1 :class="titleClass">Red</h1>
+  <p>Count is: {{ counter.count }}</p>
 </template>
 
 <script setup>
+   import { reactive, ref } from 'vue'
+
+   const counter = reactive({ count: 12 })
+   const message = ref('Hello World!')
+   const titleClass = ref('title')
 </script>
 
 <style>
+.title {
+  color: red;
+}
 </style>
